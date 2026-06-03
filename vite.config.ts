167b4513400e17658dd3d4ad2cfcd5a98909b995
Lara Lourenço <laramanonlourenco@gmail.com>
2026-06-03 14:53:49 +0100

    Tentativa de deploy

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  // Removemos o build.rollupOptions.input manualmente
-  // Deixa o Vinxi decidir o ponto de entrada
-});
