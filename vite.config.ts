b14e1f6a453740e2366d02c7516a42f9992b27c0
Lara Lourenço <laramanonlourenco@gmail.com>
2026-06-03 12:30:43 +0100

    fix: back to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  nitro: {
-    preset: "cloudflare-module",
-    rollupConfig: {
-      external: [],
-    },
-  },
 });
